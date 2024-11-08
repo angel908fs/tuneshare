3484202a08cec95ed4e0a6d4ca83d3e1dcb65b73
Rodrigo <resteves147@gmail.com>
2024-11-07 21:03:37 -0800

    saving

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use(loginRoutes);
 app.use(loadFeedRoutes);
 app.use(followUserRoutes);
 app.use(createPostRoutes);
-app.use(profileRoutes)l
+app.use(profileRoutes);
 
 app.use(defaultRoute);
 app.use(invalidRoutes); // THIS HAS TO STAY LAST
