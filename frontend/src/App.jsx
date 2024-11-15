e65f35f747818a96ca4fc2964fa0fb8ec38d78fe
Rodrigo <resteves147@gmail.com>
2024-11-14 17:20:38 -0800

    creating query in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,37 @@ import ProfilePage from './pages/Profile/ProfilePage';
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/Rightbar';
 import {Toaster} from 'react-hot-toast' // adds flare aka notifications on doing something
+import { useQuery } from '@tanstack/react-query';
+import LoadingSpinner from './components/common/LoadingSpinner';
+
+
 
 // can't get rid of sidebar and right panel without having authoried user
 function App() {
+  const {data: authUser, isLoading} = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async() => {
+      try{
+        const res = await fetch('/api/login');
+        if(!res.ok){
+          throw new Error(data.error || "Could not receieve res from login");
+        }
+        console.log("authUser is here: ", data);
+        return data;
+      }catch (error){
+        throw new Error(error);
+      }
+    },
+  });
+  if (isLoading){
+    return(
+      <div className='h-screen flex justify-center items-center'>
+        <LoadingSpinner size = 'lg'/>
+      </div>
+    )
+  }
+
+
   return (
     <div className='flex max-w-6xl mx-auto'>
       {/*common component, bc its not wrapped with routes */}
