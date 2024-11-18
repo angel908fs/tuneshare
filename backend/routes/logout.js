93b2215a275caff0d813731cfff98b00cf2a8fd8
Angel Alvarez <angel908@mail.fresnostate.edu>
2024-11-18 14:37:42 -0800

    a

--- backend/routes/logout.js
+++ backend/routes/logout.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const { generateTokenAndSetCookie } = require("../utils/generateToken.js");
 let router = express.Router();
 
-router.post('/', async (req,res) => {
+router.post('/', async (req, res) => {
     try {
-        res.status(200).send({success: "Logged Out Sucessfully"});
-    }catch (error) {
-        res.status(500).send({error: 'Internal Server Error'});
+        // even though we create and clear the cookie on the frontend we still need this here!
+        res.clearCookie('tuneshare_cookie', {
+            path: '/', 
+            secure: process.env.NODE_ENV === 'production', 
+            sameSite: 'lax', 
+        });
+        res.status(200).send({ success: "Logged Out Successfully" });
+    } catch (error) {
+        res.status(500).send({ error: 'Internal Server Error' });
     }
 });
+
 module.exports = router;
