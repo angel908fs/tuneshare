f2e3df79f87333bdaee1752e87d3e885a537487d
TheTrustedComputer <rtm518443@gmail.com>
2024-11-18 23:35:06 -0800

    Minor logic tweak

--- backend/utils/post.js
+++ backend/utils/post.js
@@ -1,77 +1,73 @@
-const Post = require("../models/post.js");
-const User = require("../models/user.js");
+    const Post = require("../models/post.js");
+    const User = require("../models/user.js");
 
-async function createPost(userID, songLink, postContent)
-{
-    try
+    async function createPost(userID, songLink, postContent)
     {
-        const newPost = new Post(
-        {   
-            user_id: userID,
-            song_link: songLink,
-            content: postContent,
-            comments: []
-        });
-        
-        await newPost.save();
-
-        // Update user's post array
-        const userPost = await User.findOne({user_id: userID});
-
-        if (!userPost)
+        try
         {
-            return { success: false, message: "User not found." };
-        }
-
-        userPost.posts.push(newPost.post_id);
-        await userPost.save();
-        
-        return { success: true, message: "Post created successfully.", data: 
-            {
+            const newPost = new Post(
+            {   
                 user_id: userID,
                 song_link: songLink,
                 content: postContent,
                 comments: []
-            }
-        };
-    }
-    catch (error)
-    {
-        // console.error("Post creation failure: ", error);
-        return { success: false, message: "Could not create the post. Please try again later.", error: error.message};
-    }
-}
-
-async function getUserPosts(username)
-{
-    try
-    {
-        // Search a username in the User collection
-        const user = await User.findOne({ username });
-
-        if (!user)
-        {
-            return { success: false, message: "This username does not exist." };
-        }
-
-        // Retrieve posts by user ID    
-        const posts = await Post.find({ user_id: user.user_id }).populate("user_id").exec();
-
-        return {
-            success: true, post: posts.map(p => ({
-                user_id: p.user_id,
-                song_link: p.song_link,
-                content: p.content,
-                comments: p.comments
-            }))
-        };
+            });
             
-    }
-    catch (error)
-    {
-        // console.error("Post retrieval failure: ", error);
-        return { success: false, message: "Could not retrieve posts. Please try again later.", error: error.message};
-    }
-}
+            await newPost.save();
 
-module.exports = { createPost, getUserPosts };
+            const userPost = await User.findOneAndUpdate(
+                { user_id: userID },
+                { $push: { posts: newPost.post_id } }
+            );
+
+            await userPost.save();
+            
+            return { success: true, message: "Post created successfully.", data: 
+                {
+                    user_id: userID,
+                    song_link: songLink,
+                    content: postContent,
+                    comments: []
+                }
+            };
+        }
+        catch (error)
+        {
+            // console.error("Post creation failure: ", error);
+            return { success: false, message: "Could not create the post. Please try again later.", error: error.message};
+        }
+    }
+
+    async function getUserPosts(username)
+    {
+        try
+        {
+            // Search a username in the User collection
+            const user = await User.findOne({ username });
+            
+            if (!user)
+            {
+                return { success: false, message: "This username does not exist." };
+            }
+            
+            // Retrieve posts by user ID    
+            const posts = await Post.find({ user_id: user.user_id }).populate("user_id").exec();
+            
+            return {
+                success: true, post: posts.map(p => ({
+                    user_id: p.user_id,
+                    song_link: p.song_link,
+                    content: p.content,
+                    comments: p.comments
+                }))
+            };
+                
+        }
+        catch (error)
+        {
+            // console.error("Post retrieval failure: ", error);
+            return { success: false, message: "Could not retrieve posts. Please try again later.", error: error.message};
+        }
+    }
+
+    module.exports = { createPost, getUserPosts };
