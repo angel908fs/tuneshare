28ad5a88334f6d495dcb27383187e70122ef3494
TheTrustedComputer <rtm518443@gmail.com>
2024-11-21 15:22:14 -0800

    Remove unused models

--- backend/routes/post_creation.js
+++ backend/routes/post_creation.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { createPost } = require("../utils/post.js");
-const Post = require("../models/post.js");
-const User = require("../models/user.js");
 
 router.post("/create-post", async (req, res) =>
 {
