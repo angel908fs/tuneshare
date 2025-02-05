49737e4ea428139d96f568ed98524b0c7e9e714a
Angel Alvarez <angel908@mail.fresnostate.edu>
2025-02-04 17:30:13 -0800

    fixed like route

--- backend/routes/likes.js
+++ backend/routes/likes.js
@@ -6,17 +6,32 @@ router.post("/like", async (req, res) => {
     if (!req.body.postID) {
         return res.status(400).send({ success: false, message: "Missing required parameter: postID" });
     }
+    if (!req.body.userID) {
+        return res.status(400).send({ success: false, message: "Missing required parameter: userID" });
+    }
 
     const postID = req.body.postID;
+    const userID = req.body.userID;
 
     try {
         const post = await Post.findOne({post_id: postID});
-        if (!post) {
-           if(!post){ return res.status(404).send({success: false, message: "Post does not exist" });}
+        const user = await User.findOne({user_id: userID})
+        if(!post) { 
+            return res.status(404).send({success: false, message: "Post does not exist" });
+        }
+        if (!user) {
+            return res.status(404).send({success: false, message: "User does not exist" });
         }
 
+        if (user.liked_posts.includes(postID)) {
+            return res.status(200).send({ success: true, message: "Post already liked by user" });
+        }
+        user.liked_posts.push(postID);
+        await user.save();
+        
         post.likes += 1;
         await post.save();
+        
         return res.status(200).send({success: true, message: "post liked successfully"})
     } catch (error) {
         return res.status(500).send({success: false, message: "Server error", error: error.message});
