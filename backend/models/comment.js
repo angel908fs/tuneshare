1973b83e124c074fd2434617b838c7ff159ed608
Angel Alvarez <angel908@mail.fresnostate.edu>
2025-02-11 17:47:41 -0800

    updated comment DB model

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');   
 
 const commentSchema = new mongoose.Schema({
+    comment_id: { type: String, default: uuidv4 }, 
     user_id: { type: String, ref: 'User', required: true },
     content: { type: String, required: true },
     created_at: { type: Date, default: Date.now }
