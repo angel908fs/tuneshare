c0a348bdc2efb9d3e91587ae0749851a266a5bc0
Rodrigo <resteves147@gmail.com>
2025-02-25 17:18:30 -0800

    added comments to code

--- frontend/src/pages/Profile/ProfilePage.jsx
+++ frontend/src/pages/Profile/ProfilePage.jsx
@@ -138,38 +138,11 @@ const ProfilePage = () => {
             )}
               <div className="flex gap-10 px-4 py-2 items-center">
                 <Link to="/">
-                  <FaArrowLeft className="w-4 h-4" />
+                  <FaArrowLeft className="w-8 h-8" />
                 </Link>
-                <div className="flex flex-col">
-                  <p className="font-bold text-lg">{userData?.username}</p>
-                  <span className="text-sm text-slate-500">
-                    {posts?.length} posts
-                  </span>
-                </div>
               </div>
               {/* COVER IMG */}
-              <div className="relative group/cover">
-                <img
-                  src={coverImg || userData?.coverImg || "cover.png"}
-                  className="h-52 w-full object-cover"
-                  alt="cover image"
-                />
-                {isMyProfile && (
-                  <div
-                    className="absolute top-2 right-2 rounded-full p-2 bg-gray-800 bg-opacity-75 cursor-pointer opacity-0 group-hover/cover:opacity-100 transition duration-200"
-                    onClick={() => coverImgRef.current.click()}
-                  >
-                    <MdEdit className="w-5 h-5 text-white" />
-                  </div>
-                )}
-
-                <input
-                  type="file"
-                  hidden
-                  accept="image/*"
-                  ref={coverImgRef}
-                  onChange={(e) => handleImgChange(e, "coverImg")}
-                />
+              <div className="flex justify-center">
                 <input
                   type="file"
                   hidden
@@ -178,7 +151,7 @@ const ProfilePage = () => {
                   onChange={(e) => handleImgChange(e, "profileImg")}
                 />
                 {/* USER AVATAR */}
-                <div className="avatar absolute -bottom-16 left-4">
+                <div className="avatar"> {/* Avatar positioning  */}
                   <div className="w-32 rounded-full relative group/avatar">
                     <img
                       src={
@@ -188,6 +161,7 @@ const ProfilePage = () => {
                       }
                       alt="Profile"
                     />
+                    {/*This is the edit pfp button */}
                     <div className="absolute top-5 right-3 p-1 bg-primary rounded-full group-hover/avatar:opacity-100 opacity-0 cursor-pointer">
                       {isMyProfile && (
                         <MdEdit
@@ -199,6 +173,7 @@ const ProfilePage = () => {
                   </div>
                 </div>
               </div>
+              {/* Edit profile button */}
               <div className="flex justify-end px-4 mt-5">
                 {isMyProfile && <EditProfileModal />}
                 {!isMyProfile && (
@@ -223,7 +198,7 @@ const ProfilePage = () => {
                 )}
               </div>
 
-              <div className="flex flex-col gap-4 mt-14 px-4">
+              <div className="flex flex-col gap-4 mt-14 px-4 items-center">
                 <div className="flex flex-col">
                   <span className="font-bold text-lg">
                     {userData?.username}
@@ -268,17 +243,6 @@ const ProfilePage = () => {
                   Posts
                   {feedType === "posts" && (
                     <div className="absolute bottom-0 w-10 h-1 rounded-full bg-primary" />
-                  )}
-                </div>
-                <div
-                  className={`flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 relative cursor-pointer ${
-                    feedType === "likes" ? "" : "text-slate-500"
-                  }`}
-                  onClick={() => setFeedType("likes")}
-                >
-                  Likes
-                  {feedType === "likes" && (
-                    <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary" />
                   )}
                 </div>
               </div>
