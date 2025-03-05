be0ee8fa3917a85f4eca372fbe8d6428e6cf3917
Angel Alvarez <angel908@mail.fresnostate.edu>
2025-03-04 17:53:12 -0800

    added comments to postjsx

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -4,7 +4,7 @@ const { v4: uuidv4 } = require('uuid');
 const commentSchema = new mongoose.Schema({
     comment_id: { type: String, default: uuidv4 }, 
     user_id: { type: String, ref: 'User', required: true },
-    content: { type: String, required: true },
+    comment: { type: String, required: true },
     created_at: { type: Date, default: Date.now }
 });
 
