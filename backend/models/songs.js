089e05a66639f0546b17379ac5dd9d9e66a58ce1
Angel Alvarez <angel908@mail.fresnostate.edu>
2025-03-25 15:30:12 -0700

    updated songs DB model

--- backend/models/songs.js
+++ backend/models/songs.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require("uuid");
 
 const songSchema = new mongoose.Schema(
     {
         song_id: { type: String, default: uuidv4 }, 
         song_link: { type: String, required: true }, // Link to the song
-        song_name:
-        artist_name:
-        album_name:
+
+        spotify_song_id: { type: String, required: true},
+        song_name: { type: String, required: true},
+        artist_names: [{ type: String, required: true}],
+        album_name: { type: String },
+        album_image_url: { type: String },
+        preview_url: { type: String }, // 30-sec preview
+        duration_ms: { type: Number }, // track length
+        explicit: { type: Boolean },
+        deezer_preview_url: { type: String },
         
         created_at: { type: Date, default: Date.now }
     }
