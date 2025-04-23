4d0a9533a8bba1826e5051a6bd0422a743bd8db9
suadaly <suadaly@mail.fresnostate.edu>
2025-04-22 17:46:31 -0700

    added admin routes

---
+++ backend/routes/admin_routes.js
@@ -0,0 +1,41 @@
+const User = require("../models/user.js");
+const Post = require("../models/post.js");
+
+
+router.post("/get-users",  async (req, res) => { 
+    try {
+        const Users = await User.find()
+        .limit(1000);
+        
+        return res.status(200).send({
+            success: true,
+            message: "users retrieved successfully",
+            data: {
+                users: Users
+            }
+        })
+        } catch (error) {
+        return res.status(500).send({ success: false, message: "server error", error: error.message });
+    }
+});
+
+const User = require("../models/user.js");
+const Post = require("../models/post.js");
+
+
+router.post("/get-posts",  async (req, res) => { 
+    try {
+        const Users = await User.find()
+        .limit(1000);
+        
+        return res.status(200).send({
+            success: true,
+            message: "users retrieved successfully",
+            data: {
+                users: Users
+            }
+        })
+        } catch (error) {
+        return res.status(500).send({ success: false, message: "server error", error: error.message });
+    }
+});
